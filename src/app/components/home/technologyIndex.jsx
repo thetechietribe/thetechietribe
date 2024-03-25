9c0a4d69e25fbd4f0d1e4b777e033202fcf1dbbc
minhaj-ops <minhaj.alvi@thetechietribe.com>
2024-03-25 16:47:42 -0400

    Slider added

--- src/app/components/home/technologyIndex.jsx
+++ src/app/components/home/technologyIndex.jsx
@@ -1,18 +1,23 @@
-import React from "react";
-import Image from "next/image";
+import { Box } from "@mui/material";
+import * as React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
-import { Box, Typography } from "@mui/material";
-
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 import ReactIcon from "../../../../public/assets/pngs/technologies/React.png";
 import Angular from "../../../../public/assets/pngs/technologies/Angular.png";
+import NextIcon from "../../../../public/assets/pngs/technologies/Next.png";
+import Vue from "../../../../public/assets/pngs/technologies/Vue.png";
 import AWS from "../../../../public/assets/pngs/technologies/AWS.png";
 import AWSAmplify from "../../../../public/assets/pngs/technologies/AWS-Amplify.png";
+import Rails from "../../../../public/assets/pngs/technologies/Rails.png";
 import GoogleCloud from "../../../../public/assets/pngs/technologies/Google-Cloud.png";
 import Laravel from "../../../../public/assets/pngs/technologies/Laravel.png";
-import NextIcon from "../../../../public/assets/pngs/technologies/Next.png";
 import PostGres from "../../../../public/assets/pngs/technologies/PostGres.png";
-import Rails from "../../../../public/assets/pngs/technologies/Rails.png";
-import Vue from "../../../../public/assets/pngs/technologies/Vue.png";
+import Image from "next/image";
 
 const icons = [
   ReactIcon,
@@ -27,61 +32,55 @@ const icons = [
   PostGres,
 ];
 
-const styles = {
-  mainContainer: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    padding: { xs: "40px", lg: "80px 80px 0px 80px" },
-    backgroundColor: "#ffffff",
-    alignItems: "center",
-  },
-
-  heading: {
-    fontSize: "20px",
-    fontFamily: "Poppins",
-    fontWeight: "400",
-    color: "#378C92",
-    marginBottom: "20px",
-    textDecoration: "underline",
-    textDecorationColor: "#313431",
-    textDecorationThickness: "2px",
-    textUnderlineOffset: "4px",
-  },
-
-  iconContainer: {
-    display: "flex",
-    justifyContent: "center",
-    backgroundColor: "red",
-  },
-
-  icon: {
-    width: "100px",
-    height: "auto",
-    margin: "0 10px",
-  },
-};
-
-const TechnologyIndex = () => {
+const Testimonials = () => {
   return (
-    <Box sx={styles.mainContainer}>
-      <Typography sx={styles.heading}>TECHNOLOGIES WE ARE USING</Typography>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: { xs: "300px", lg: "100%" },
-          overflow: { xs: "scroll", lg: "auto" },
-          mt: "1rem",
-          alignItems: "center",
-        }}
-      >
-        {icons.map((data, index) => (
-          <Image key={index} src={data} alt="icon" width={80} />
-        ))}
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        padding: { xs: "40px", lg: "80px 80px 0px 80px" },
+        justifyContent: "center",
+        backgroundColor: "#ffffff",
+        alignItems: "center",
+      }}
+    >
+      <Box sx={{ width: "100%" }}>
+        <Swiper
+          spaceBetween={30}
+          slidesPerView={8}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          modules={[Pagination, Navigation, Autoplay]}
+        >
+          {icons.map((icon, index) => (
+            <SwiperSlide key={index}>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  gap: "1rem",
+                  alignItems: "center",
+                }}
+              >
+                {icon === NextIcon ? (
+                  <Image
+                    src={icon}
+                    alt={`Technology icon ${index}`}
+                    width={80}
+                  />
+                ) : (
+                  <Image src={icon} alt={`Technology icon ${index}`} />
+                )}
+              </Box>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </Box>
     </Box>
   );
 };
 
-export default TechnologyIndex;
+export default Testimonials;
